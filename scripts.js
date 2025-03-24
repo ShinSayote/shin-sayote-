61509a7165b2e97ce0352f52e7af0793baadbce2
ShinSayote <japs.zenr01@gmail.com>
2025-03-24 23:43:12 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,3 @@
+document.getElementById('inquiryButton').addEventListener('click', function() {
+    alert("Thank you for your interest! Please contact me via email at james@clixphotography.com for inquiries.");
+});
